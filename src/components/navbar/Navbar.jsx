bdb3ce62acb87a6fcc4ff28a4543a8ab28fc34c1
Adesina Joshua <tolujosh1@gmail.com>
2024-09-09 21:57:46 +0100

    adding neccesary libs and middleware

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,21 @@
-const Navbar =() => {
-    return (
-        <div>Navbar</div>
-    );
-};
 
-export default Navbar;
+import Link from "next/link"
+import Links from "./links/Links"
+import styles from "./navbar.module.css"
+import { auth } from "@/lib/auth";
+
+const Navbar = async () => {
+
+  const session = await auth();
+
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>Logo</Link>
+      <div>
+        <Links session={session}/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
