310dfab700a84ec588a451e58f8d29362e1e1085
Adesina Joshua <tolujosh1@gmail.com>
2024-09-12 15:29:09 +0100

    creating other fundamentals

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images:{
+      remotePatterns:[
+        {
+          protocol: 'https',
+          hostname: "images.pexels.com"
+        }
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
+  
--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,3 @@
-
 import NextAuth from "next-auth";
 import { authConfig } from "./lib/auth.config";
 
@@ -7,5 +6,3 @@ export default NextAuth(authConfig).auth;
 export const config = {
   matcher: ["/((?!api|static|.*\\..*|_next).*)"],
 };
-
-// FOR MORE INFORMATION CHECK: https://nextjs.org/docs/app/building-your-application/routing/middleware
